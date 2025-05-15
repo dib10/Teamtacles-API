ee1bb27ea3ef2a28f653ff8521c7bcc792c07e6d
Caio <caiodib10@gmail.com>
2025-05-15 13:08:41 -0300

    🎉 Inicialização: Estrutura do projeto Spring Boot criada

---
+++ teamtacles-api/src/main/java/com/teamtacles/teamtacles_api/TeamtaclesApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamtacles.teamtacles_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtaclesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtaclesApiApplication.class, args);
+	}
+
+}
---
+++ teamtacles-api/src/test/java/com/teamtacles/teamtacles_api/TeamtaclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtacles.teamtacles_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtaclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

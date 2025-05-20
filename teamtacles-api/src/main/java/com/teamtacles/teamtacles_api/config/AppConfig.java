599f5f6fafc3043846626b57a36d7b5625c1c715
Caio <caiodib10@gmail.com>
2025-05-20 20:39:27 -0300

    ✨ feat: Add AppConfig.java

---
+++ teamtacles-api/src/main/java/com/teamtacles/teamtacles_api/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.teamtacles.teamtacles_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
+    
+

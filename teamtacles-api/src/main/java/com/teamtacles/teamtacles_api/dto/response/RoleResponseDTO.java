2897eabd01e7a62a8cda4a32329b0a29ad4f47f4
Caio <caiodib10@gmail.com>
2025-05-30 13:44:24 -0300

    style: 💄 Add Lombok constructors to RoleResponseDTO

--- teamtacles-api/src/main/java/com/teamtacles/teamtacles_api/dto/response/RoleResponseDTO.java
+++ teamtacles-api/src/main/java/com/teamtacles/teamtacles_api/dto/response/RoleResponseDTO.java
@@ -3,10 +3,12 @@ package com.teamtacles.teamtacles_api.dto.response;
 import com.teamtacles.teamtacles_api.model.enums.ERole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class RoleResponseDTO {
     private Long id;
     private ERole roleName;
-    
 }

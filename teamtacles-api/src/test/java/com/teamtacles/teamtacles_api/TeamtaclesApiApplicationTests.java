533ff460864d99257255499c83f328f1bd216b24
Gabriela Santana <gabisantanadev@gmail.com>
2025-06-02 23:44:48 -0300

    🔧✨ Add @ActiveProfiles(test) annotation

--- teamtacles-api/src/test/java/com/teamtacles/teamtacles_api/TeamtaclesApiApplicationTests.java
+++ teamtacles-api/src/test/java/com/teamtacles/teamtacles_api/TeamtaclesApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.teamtacles.teamtacles_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TeamtaclesApiApplicationTests {
 	@Test
 		void contextLoads() {
